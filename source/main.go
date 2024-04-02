c103e31941b1da4b53b2252f9c4f0e7d80d433cc
chrisgbradley <chris@chrisgbradley.com>
2024-04-02 08:18:31 -0400

    refactor and add htmx support

---
+++ source/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Renderer = newTemplate()
+
+	e.Static("/css", "css")
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", 0)
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
